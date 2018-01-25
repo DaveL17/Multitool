34cbcce5f880b0a27ed5f1ce332ced3ccf76ad3f
DaveL17 <davel17@users.noreply.github.com>
2018-01-25 06:53:14 -0600

    Corrects master branch for lost files from shared branch. Master branch is now the only branch.

--- Multitool.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Multitool.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -39,7 +39,7 @@ __copyright__ = Dave.__copyright__
 __license__   = Dave.__license__
 __build__     = Dave.__build__
 __title__     = 'Multitool Plugin for Indigo Home Control'
-__version__   = '1.0.13'
+__version__   = '1.0.14'
 
 # =============================================================================
 
